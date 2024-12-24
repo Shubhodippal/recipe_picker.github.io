4a3cc9c756e1b7470c88f2bc20b16b598c6c7bfc
Shubhodippal <shubhodippal01@gmail.com>
2024-12-24 13:21:29 +0530

    retest2.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f5255ceb89312e9994b77176c7e1ee24
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "153fd11d177722ae743647b84023857d",
+"flutter_bootstrap.js": "01e30f3b7deb81697de1f6f94cd9bd4b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
